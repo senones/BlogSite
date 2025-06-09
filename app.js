6df7515c66e8c35e7ed8cdb05d1aec239eadb3df
Senon <59428039+senones@users.noreply.github.com>
2025-06-09 02:57:46 +0200

    Update app.js
    changed port to const port = process.env.PORT || 3000;
    for rendering.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
